d4484eabbd9b46b1aa2e42446b4fb58dcd893c75
Piyush Goenka <goenkapiyush2005@gmail.com>
2025-11-06 18:26:25 +0530

    implement cocktails section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react'
 
-import { navLinks } from '../../contants'
+import { navLinks } from '../../constants'
 
 const Navbar = () => {
  useGSAP(() => {
